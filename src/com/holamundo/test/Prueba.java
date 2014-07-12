6b048e6a37439bb5a713317c8e376149b38f5c35
Jose <pp0428>
2014-07-12 12:03:51 -0500

    prueba

--- src/com/holamundo/test/Prueba.java
+++ src/com/holamundo/test/Prueba.java
@@ -2,7 +2,7 @@ package com.holamundo.test;
 
 public class Prueba {
 	public static void main(String[] args) {
-		int x=12;
+		int x=10;
 		String ola="hola sapo cerdo mono koala";
 		System.out.println(ola);
 	}
